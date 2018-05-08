aebe4bad655fa8689cec0a6934aec3fa07c94cfd
TalOmari <talomari592@gmail.com>
2018-05-08 18:53:46 +0300

    lab Techncians

--- server/server.js
+++ server/server.js
@@ -9,9 +9,8 @@ var userRouter = require('./resources/User/userRouter');
 var mdeicalRouter = require('./resources/MedicalRecorde/MedicalRouter');
 var DepartmentsRouter = require('./resources/Departments/DepartmentsRouter');
 var DoctorRouter = require('./resources/Doctor/DoctorRouter');
-
-
 var labRouter=require('./resources/LabsResult/labRouter')
+var labsTechnciansRouter=require('./resources/LabsTechncians/labsTechnciansRouter')
 
 var session = require('express-session');
 var bcrypt = require('bcrypt-nodejs');
@@ -25,12 +24,12 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(express.static(__dirname + '/../react-client/dist'));
 app.use('/dept',DepartmentsRouter);
-app.use('/',userRouter);
+//app.use('/',userRouter);
 
 app.use('/api/medical',mdeicalRouter);
 app.use('/Doctor',DoctorRouter);
-
 app.use('/res',labRouter);
+app.use('/tech',labsTechnciansRouter)
 
 app.get('*', function (req, res){
     res.sendFile(path.resolve(__dirname, '../react-client/dist', 'index.html'));
