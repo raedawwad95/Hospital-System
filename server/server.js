4b7360ece552e743dd1478a7d6b0aafcca6e6bca
AhmadAlYaaqba <ahmad.y91@hotmail.com>
2018-05-09 09:41:28 +0300

    make 4 sperate react folders

--- server/server.js
+++ server/server.js
@@ -26,6 +26,8 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(express.static(__dirname + '/../react-client/dist'));
 app.use('/admin', express.static(__dirname + '/../react-Admin/dist'));
+app.use('/doctors', express.static(__dirname + '/../react-Doctors/dist'));
+app.use('/labs', express.static(__dirname + '/../react-Labs/dist'));
 app.use('/dept',DepartmentsRouter);
 //app.use('/',userRouter);
 
