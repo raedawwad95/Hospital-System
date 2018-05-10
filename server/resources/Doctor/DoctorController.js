390173c0ed9ff4bc01d58b5c438753d17f368fbb
raedawwad95 <awwad12121@gmail.com>
2018-05-10 16:58:50 +0300

    updateDoctor

--- server/resources/Doctor/DoctorController.js
+++ server/resources/Doctor/DoctorController.js
@@ -1,17 +1,44 @@
 var Doctor = require('./Doctor.js');
 
+//retrieve .. find and retrieve data
 exports.retrieve=function(req,res){
 	Doctor.find({},function(err,data){
 		if(err){
 			res.send(err);
 		}
 		res.json(data);
-	})
+	});
 }
 
+// create new Doctor 
 exports.create=function(req,res){
+
 	var doctor =new Doctor(req.body);
-	doctor.save(req.body,function(err,data){
+	console.log(doctor)
+	doctor.save(function(err,data){
+		console.log(data)
 		res.json(data)
-	})
+	});
 }
+
+//Update doctor information 
+exports.update=function(req,res){
+	var updDoc ={
+	password: req.body.password,
+	fullName: req.body.fullName,
+	imageOfDoctor: req.body.imageOfDoctor,
+	imageOfId: req.body.imageOfId,
+	nationalId: req.body.nationalId,
+	hospitalId: req.body.hospitalId,
+	spicilityStatus: req.body.spicilityStatus,
+
+	}
+	//update doctor information 
+	Doctor.update({'userName':"raedawwwwad"},
+		{$set:updDoc},
+		function(err,data){
+		res.json(data);
+	});
+}
+
+
--- server/resources/Doctor/DoctorRouter.js
+++ server/resources/Doctor/DoctorRouter.js
@@ -2,11 +2,17 @@ var doctorRouter =require('express').Router();
 var DoctorController = require('./DoctorController.js');
 
 doctorRouter.route('/')
+//retrieve doctors data
 .get(function(req,res){
 	DoctorController.retrieve(req,res);
 })
+//save Doctor to a database
 .post(function(req,res){
 	DoctorController.create(req,res);
+})
+//update doctor
+.put(function(req,res){
+	DoctorController.update(req,res);
 })
 
 module.exports = doctorRouter;
