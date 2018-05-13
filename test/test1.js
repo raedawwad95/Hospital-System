408e03b7ab49f769c79caa4ee0191b275b385208
montheramer <monther.amer48@gmail.com>
2018-05-13 19:37:33 +0300

    test the get request from doctors

--- test/test1.js
+++ test/test1.js
@@ -3,9 +3,9 @@ var expect    = chai.expect;
 var request   = require('request'); 
 var mongoose  = require('mongoose');
 var Dept      = require('../server/resources/Departments/Departments');
+var Doc       = require('../server/resources/Doctor/Doctor');
 
-
-describe('Testing the routes', function(){
+describe('test the get request from Departments', function(){
 
     before('connect', function(){
         return mongoose.createConnection('mongodb://localhost/dept')
@@ -31,3 +31,43 @@ describe('Testing the routes', function(){
         });
     });
 });
+
+
+describe('test the get request from Doctors', function(){
+
+    before('connect', function(){
+        return mongoose.createConnection('mongodb://localhost/Doctor')
+    })
+
+    beforeEach(function(){
+        return Doc.remove({})
+    })
+
+    beforeEach(function(){
+        var newDoc = new Doc();
+            newDoc.userName='monther'
+            newDoc.password='1234'
+            newDoc.fullName='Monther Amer'
+            newDoc.imageOfDoctor='kjasjdkjask'
+            newDoc.imageOfId='asdasd'
+            newDoc.nationalId='123'
+            newDoc.hospitalId='1234'
+            newDoc.spicilityStatus='m'
+            newDoc.hoursOfWork='8'
+            newDoc.gender='Male'
+            newDoc.doctorType='D'
+            newDoc.department='1'
+        return newDoc.save();
+    });
+
+    it('should list all Doctors', function(done){
+        var url = 'http://localhost:3000/Doctor/retrieve';
+        request.get(url, (error, response, body) => {
+            if (error) done(error)
+            expect(response).to.be.an('Object');
+            done();
+        });
+    });
+});
+
+
