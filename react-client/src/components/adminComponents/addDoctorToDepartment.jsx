b31ef469a4e65891cc807463c4086cb7abf0906d
AhmadAlYaaqba <ahmad.y91@hotmail.com>
2018-05-13 16:11:24 +0300

    Doctor to department Done

--- react-client/src/components/adminComponents/addDoctorToDepartment.jsx
+++ react-client/src/components/adminComponents/addDoctorToDepartment.jsx
@@ -1,13 +1,52 @@
 import React from 'react';
 import $ from 'jquery';
-import {Button} from 'react-bootstrap';
+import { withStyles, MenuItem, TextField, Input, InputLabel, InputAdornment,
+		FormControl, Paper, Grid, CardHeader, FormHelperText,
+		Button, Select, Card, CardActions, CardContent } from 'material-ui';
+import PropTypes from 'prop-types';
+
+	const styles = theme => ({
+	  container: {
+	    display: 'flex',
+	    flexWrap: 'wrap',
+	  },
+	  textField: {
+	    width: 200,
+	  },
+	  menu: {
+	    width: 200,
+	  },
+	  paper: {
+    	padding: theme.spacing.unit * 2,
+    	textAlign: 'center',
+    	color: theme.palette.text.secondary,
+  	  },
+  	  margin: {
+    	margin: theme.spacing.unit,
+  	  },
+  	  input: {
+	    display: 'none',
+	  },
+	  formControl: {
+	    margin: theme.spacing.unit,
+	    minWidth: 120,
+	  },
+	  selectEmpty: {
+	    marginTop: theme.spacing.unit * 2,
+	  },
+	  card: {
+	    minWidth: 275,
+	  },
+
+	});
 
 class addDoctorToDepartment extends React.Component{
 	constructor(props){
 		super(props);
 		this.state={
 			depts:[],
-			docId:''
+			docId:'',
+			selected:''
 		}
 		this.handleChange=this.handleChange.bind(this);
 		this.add=this.add.bind(this)
@@ -30,16 +69,15 @@ class addDoctorToDepartment extends React.Component{
 
     handleChange(e){
     	this.setState({
-    		docId:e.target.value
-    	})
-    	
+			[e.target.name]:e.target.value
+		});
     }
 
     add(){
     	var that=this;
     	var idOfDept=$('select[name=selector]').val();
     	var obj={
-    		idOfDept:idOfDept,
+    		idOfDept:that.state.selected,
     		docId:that.state.docId
     	}
     	$.ajax({
@@ -54,22 +92,67 @@ class addDoctorToDepartment extends React.Component{
     }
 
 	render(){
-
+		const { classes } = this.props;
 		return(
 		<div>
-			<select name='selector'>
-				{this.state.depts.map(function(item){
-					return(
-						<option value={item.idOfDept}>{item.nameOfDept}</option>
-					)
-				})}			
-			</select>
-			<input type='text' value={this.state.docId} onChange={this.handleChange} placeholder='add doctor'/>
-			<Button onClick={this.add}> Add Doc</Button>
+			<Card className={classes.card}>
+				<CardHeader
+			        title="Add doctor to Department"
+			      />
+					<CardContent>
+						<Grid container spacing={24}>
+							<Grid item xs={6} sm={3}></Grid>
+							<Grid item xs={6} sm={3}>
+								<FormControl className={classes.formControl}>
+						          <InputLabel htmlFor="add-Department">Department</InputLabel>
+						          <Select
+						            value={this.state.selected}
+						            onChange={this.handleChange}
+						            input={<Input name="selected" id="add-Department" />}
+						          >
+						            <MenuItem value="">
+						              <em>None</em>
+						            </MenuItem>
+						            {this.state.depts.map(function(item){
+										return(
+											<MenuItem value={item.idOfDept}>{item.nameOfDept}</MenuItem>
+										)
+									})}	
+						          </Select>
+						          <FormHelperText>please select your gender</FormHelperText>
+						        </FormControl>
+							</Grid> 
+							<Grid item xs={6} sm={3}>
+								<TextField
+						          required
+						          id="Add Doctor"
+						          label="Add Doctor"
+						          placeholder="Add Doctor"
+						          className={classes.textField}
+						          margin="normal"
+						          value={this.state.docId}
+						          name="docId"
+				          		  onChange={this.handleChange}
+						        />
+							</Grid> 
+							<Grid item xs={6} sm={3}></Grid>
+							<Grid item xs={10} sm={5}></Grid>
+							<CardActions>
+								<Button variant="raised" color="primary" className={classes.button} onClick={this.add} >
+						        	Submit
+						      	</Button>
+							</CardActions>
+						</Grid>
+					</CardContent>
+				</Card>
 		</div>
 			)
 	}
 
 }
 
-export default addDoctorToDepartment;
+addDoctorToDepartment.propTypes = {
+  classes: PropTypes.object.isRequired,
+};
+
+export default withStyles(styles)(addDoctorToDepartment);
--- react-client/src/components/adminComponents/adminMain.jsx
+++ react-client/src/components/adminComponents/adminMain.jsx
@@ -3,7 +3,8 @@ import AddLabTechncians from './AddLabTechncians.jsx';
 import addDept from './addDept.jsx';
 import AdminNavbar from './AdminNavbar.jsx';
 import AdminHome from './AdminHome.jsx';
-import {browserHistory, Route, BrowserRouter as Router, Link} from 'react-router-dom';
+import addDoctorToDepartment from './addDoctorToDepartment.jsx';
+import { browserHistory, Route, BrowserRouter as Router, Link } from 'react-router-dom';
 class Admin extends React.Component {
   constructor(props) {
     super(props);
@@ -16,6 +17,7 @@ class Admin extends React.Component {
         <Route exact path="/admin" component={AdminHome} />
         <Route path="/admin/AddLabTechncians" component={AddLabTechncians} />
         <Route path="/admin/addDept" component={addDept} />  
+        <Route path="/admin/addDoctorToDepartment" component={addDoctorToDepartment} />  
       </div>
     )
   }
