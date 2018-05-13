6253e4321877891c69085ff560d1dfab4e7ed480
AhmadAlYaaqba <ahmad.y91@hotmail.com>
2018-05-13 14:43:49 +0300

    continue admin components styling

---
+++ react-client/src/components/adminComponents/AdminHome.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+class AdminHome extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+  render() {
+    return (
+      <div>
+        <h1>Admin Home Page</h1>
+        <h3> TODO: </h3>
+        <p> add notification here </p>
+      </div>
+    )
+  }
+}
+export default AdminHome;  
--- react-client/src/components/adminComponents/adminMain.jsx
+++ react-client/src/components/adminComponents/adminMain.jsx
@@ -1,7 +1,8 @@
 import React from 'react';
 import AddLabTechncians from './AddLabTechncians.jsx';
 import addDept from './addDept.jsx';
-import AdminNavbar from './AdminNavbar.jsx'
+import AdminNavbar from './AdminNavbar.jsx';
+import AdminHome from './AdminHome.jsx';
 import {browserHistory, Route, BrowserRouter as Router, Link} from 'react-router-dom';
 class Admin extends React.Component {
   constructor(props) {
@@ -11,8 +12,8 @@ class Admin extends React.Component {
     return (
       <div>
       <AdminNavbar/>
-        <h1>Admin PAge </h1>
-
+      <br/>
+        <Route exact path="/admin" component={AdminHome} />
         <Route path="/admin/AddLabTechncians" component={AddLabTechncians} />
         <Route path="/admin/addDept" component={addDept} />  
       </div>
