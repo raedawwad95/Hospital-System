a40f14ecc64a5153a2371ebb22c599878a3cb320
TalOmari <talomari592@gmail.com>
2018-05-16 17:31:34 +0300

    ediding files beacuse of conflict

---
+++ reactTest/test.js
@@ -0,0 +1,20 @@
+ var chai = require('chai')
+ var expect = chai.expect;
+ var assert = require('chai').assert
+ import React from 'react';
+ import ReactTestUtils from 'react-dom/test-utils'; 
+ import Home from '../react-client/src/components/Home';
+ import ShallowRenderer from 'react-test-renderer/shallow';
+ import sinon from 'sinon';
+ import { configure } from 'enzyme';
+ import { shallow } from 'enzyme';
+ import Adapter from 'enzyme-adapter-react-15';
+ configure({ adapter: new Adapter() });
+
+
+
+
+
+it('should be class component ',function(){
+	expect(React.Component.isPrototypeOf(Home)).to.be.true;
+})
