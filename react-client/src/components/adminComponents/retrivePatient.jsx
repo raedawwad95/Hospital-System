96a3799f3009565088479a4fa5cabfd70abe58ea
raedawwad95 <awwad12121@gmail.com>
2018-05-19 18:31:49 +0300

    retrive Image Medical And Lab Result Admin Comp

--- react-client/src/components/adminComponents/retrivePatient.jsx
+++ react-client/src/components/adminComponents/retrivePatient.jsx
@@ -2,16 +2,35 @@ import React from 'react';
 import $ from 'jquery';
 import { TextField, Grid,
 		Button,CardActions} from 'material-ui';
+import Modal from 'react-modal';
 
+const customStyles1 = {
+  content : {
+    top                   : '40%',
+    left                  : '50%',
+    right                 : '10%',
+    bottom                : '10%',
+    marginRight           : '-50%',
+    transform             : 'translate(-50%, -50%)'
+  }
+};
 class retrivePatient extends React.Component{
 	constructor(props){
 		super(props);
 		this.state={
 			userData:[],
 			username:'',
+			modalIsOpenImageMedical: false, 
+    		modalIsOpenImageResult:false,
+    		imageMedical:'',
+    		imageResult:''
 		}
 		this.onChange=this.onChange.bind(this);
 		this.retriveData=this.retriveData.bind(this);
+	    this.openModalImageMedical = this.openModalImageMedical.bind(this);
+	    this.closeModalImageMedical = this.closeModalImageMedical.bind(this);
+	    this.openModalImageResult = this.openModalImageResult.bind(this);
+	    this.closeModalImageResult = this.closeModalImageResult.bind(this);
 	}
 	onChange(e){
 		this.setState({
@@ -31,7 +50,26 @@ class retrivePatient extends React.Component{
  		}
 		});
     }
+    closeModalImageMedical() {
+		this.setState({modalIsOpenImageMedical: false});
+   }
+    openModalImageMedical(e) {
+		this.setState({
+			modalIsOpenImageMedical: true,
+			imageMedical:e.target.value
+		});
+    }
+    closeModalImageResult(){
+    	this.setState({modalIsOpenImageResult: false});
+    }
+    openModalImageResult(e) {
+    	this.setState({
+	    	modalIsOpenImageResult: true,
+	    	imageResult:e.target.value
+	    });
+    }
 render(){
+	var that =this;
 	if(this.state.userData.length>0){
 
 
@@ -101,6 +139,7 @@ render(){
 		        <th>Medical Examination Time</th>
 		        <th>Result Entry Time</th>
 		        <th>Description</th>
+		        <th>Image Result</th>
 		      </tr>
 		    </thead>		    
 		    <tbody style={{textAlign:'center'}}>
@@ -112,6 +151,7 @@ render(){
 			        <td>{item.medicalExaminationTime}</td>
 			        <td>{item.resultEntryTime}</td>
 			        <td>{item.description}</td>
+			       <td><button value ={item.imageOfResult} onClick={that.openModalImageResult}>{item.imageOfResult}</button></td>
 		         </tr>
 		         )
             })}
@@ -119,6 +159,27 @@ render(){
          </table>
          </div>
 	    </div>
+	     <Modal
+          isOpen={this.state.modalIsOpenImageResult}
+          contentLabel="Add New Record"
+          style={customStyles1}
+        >
+ 		<div className="card">
+		<div className='container-fluid'>
+	
+	    <Grid container spacing={24}>
+	    <Grid item xs={24} sm={12}>
+	        <img  style={{textAlign:'center'}} src ={this.state.imageResult}></img>
+		<CardActions>
+		    <Button variant="raised" color="primary" onClick={this.closeModalImageResult} >
+	        	Close
+	      	</Button>
+		</CardActions>	
+		</Grid>
+		</Grid>
+		</div>
+		</div>
+        </Modal>
 	    <br />
 	    <div className="card">
 	    <div className='container-fluid'>
@@ -129,6 +190,7 @@ render(){
 		        <th width='20%'>Id </th>
 		        <th>Doctor Name</th>
 		        <th>Description</th>
+		        <th>Image Medical</th>
 		      </tr>
 		    </thead>		    
 		    <tbody style={{textAlign:'center'}}>
@@ -138,6 +200,7 @@ render(){
 			        <td>{item._id}</td>
 			        <td>{item.doctorId.fullName}</td>
 			        <td>{item.description}</td>
+			        <td value ={item.image}><button value ={item.image} onClick={that.openModalImageMedical}>{item.image}</button></td>
 		         </tr>
 		         )
             })}
@@ -145,6 +208,27 @@ render(){
          </table>
 	    </div>
 	    </div>
+	     <Modal
+          isOpen={this.state.modalIsOpenImageMedical}
+          contentLabel="Add New Record"
+          style={customStyles1}
+        >
+ 		<div className="card">
+		<div className='container-fluid'>
+	
+	    <Grid container spacing={24}>
+	    <Grid item xs={24} sm={12}>
+	        <img  style={{textAlign:'center'}} src ={this.state.imageMedical}></img>
+		<CardActions>
+		    <Button variant="raised" color="primary" onClick={this.closeModalImageMedical} >
+	        	Close
+	      	</Button>
+		</CardActions>	
+		</Grid>
+		</Grid>
+		</div>
+		</div>
+        </Modal>
 	    <br />
 	    </div>
 	    
