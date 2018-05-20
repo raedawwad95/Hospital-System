eb3890068adb21be3c909c7e38fea32fc9894fc9
TalOmari <talomari592@gmail.com>
2018-05-20 22:25:11 +0300

    delete lab tech component

---
+++ react-client/src/components/adminComponents/DeleteLabTech.jsx
@@ -0,0 +1,23 @@
+import React, { Component } from 'react';
+import $ from "jquery";
+
+ class DeleteLabTech extends Component {
+ 	constructor(props){
+ 		super(props);
+ 		this.state={
+ 			username:""
+ 		}
+ 	}
+
+ 	
+
+	render() {
+		return (
+			<div></div>
+		);
+	}
+}
+
+
+
+export default DeleteLabTech;
