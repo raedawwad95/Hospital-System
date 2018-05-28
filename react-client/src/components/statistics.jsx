aeac910f041a6714ada08cfc9cdf08700c472124
TalOmari <talomari592@gmail.com>
2018-05-28 12:26:52 +0300

    add statistic component

---
+++ react-client/src/components/statistics.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+class Statistics extends Component {
+	constructor(props){
+		super(props);
+		this.state={
+			
+		}
+	}
+	render() {
+		return (
+			<div></div>
+		);
+	}
+}
+
+export default Statistics;
