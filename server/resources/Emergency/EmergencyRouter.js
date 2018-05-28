2d5e0a0f8dcf92f1c6e4be6137f12419c2b53026
AhmadAlYaaqba <ahmad.y91@hotmail.com>
2018-05-28 12:38:36 +0300

    create emergency call backend ready

--- server/resources/Emergency/EmergencyRouter.js
+++ server/resources/Emergency/EmergencyRouter.js
@@ -3,7 +3,7 @@ var EmergencyController = require('./EmergencyController');
 
 // create emergency call 
 EmergencyRouter.route('/')
-	.delete(function (req, res) {
+	.post(function (req, res) {
 		EmergencyController.create(req, res);
 	})
 
