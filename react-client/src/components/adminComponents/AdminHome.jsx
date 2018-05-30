e1e56b00e11f0f68bb2a1eccaf6bcb9e805634dd
raedawwad95 <awwad12121@gmail.com>
2018-05-30 17:33:57 +0300

    EmergencyWhenLogin

--- react-client/src/components/adminComponents/AdminHome.jsx
+++ react-client/src/components/adminComponents/AdminHome.jsx
@@ -5,15 +5,47 @@ import RetriveEmergency from './RetriveEmergency.jsx';
 class AdminHome extends React.Component {
   constructor(props) {
     super(props);
+  this.state={
+      auth: false
+    }
+  }
+  componentDidMount() {
+    var that = this
+    $.ajax({
+      url:'/itDep/isLogin',
+      type:'GET',
+      success:function(data){
+        that.setState({
+          auth: true
+        })
+      },
+      error:function(err){
+        console.log(err);
+      }
+    });
+
   }
   render() {
+    const { auth } = this.state;
     return (
       <div>
+      {auth &&(
+        <div>
+          <h1>Admin Home Page</h1>
+          <h3> TODO: </h3>
+          <p> add notification here </p>
+          <RetriveEmergency />
+        </div>
+        )}
+      {!auth &&(
+        <div>
         <h1>Admin Home Page</h1>
         <h3> TODO: </h3>
         <p> add notification here </p>
         <RetriveEmergency />
         <Statistics/>
+        </div>
+        )}
       </div>
     )
   }
